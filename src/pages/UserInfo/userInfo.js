2ca6f773b0cc6a323f70282a78830ce64ee823f4
chenzl <2191403349@qq.com>
2018-02-12 21:32:47 +0800

    更新配置

--- src/pages/UserInfo/userInfo.js
+++ src/pages/UserInfo/userInfo.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {getUserInfo} from 'actions/userInfo';
 
+import './userInfo.css';
 import imgAvatar from 'img/avatar.jpg';
 
 class UserInfo extends Component {
@@ -12,8 +13,8 @@ class UserInfo extends Component {
                 {
                     isLoading ? '请求信息中......' : (
                         errorMsg ? errorMsg :
-                            <div>
-                                <p id="test">用户信息</p>
+                            <div className="user-info-box">
+                                <h3 id="test">用户信息</h3>
                                 <p>用户名：{userInfo.name}</p>
                                 <p>介绍：{userInfo.intro}</p>
                                 <img src={imgAvatar} alt="这是头像"/>
